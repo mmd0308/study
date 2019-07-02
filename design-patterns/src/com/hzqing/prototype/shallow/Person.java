a3ed8614d4de8f3543dcc093cc92c404b61616b2
hengzhaoqing <hengzhaoqing@163.com>
2019-07-02 11:53:33 +0800

    feat:原型设计模式代码案例

---
+++ design-patterns/src/com/hzqing/prototype/shallow/Person.java
@@ -0,0 +1,30 @@
+package com.hzqing.prototype.shallow;
+
+/**
+ * @author hzqing
+ * @date 2019-07-02 11:16
+ */
+public class Person implements Cloneable{
+    private String name;
+
+    private Compony compony;
+
+
+    public Person(String name, Compony compony) {
+        this.name = name;
+        this.compony = compony;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Compony getCompony() {
+        return compony;
+    }
+}
