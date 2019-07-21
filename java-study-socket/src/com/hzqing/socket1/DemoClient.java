1fca2cd9d87a8fd49feb0ca65da0556e628c0928
hengzhaoqing <hengzhaoqing@163.com>
2019-07-21 21:14:07 +0800

    feat:rpcdemo

---
+++ java-study-socket/src/com/hzqing/socket1/DemoClient.java
@@ -0,0 +1,8 @@
+package com.hzqing.socket1;
+
+/**
+ * @author hzqing
+ * @date 2019-07-16 07:43
+ */
+public class DemoClient {
+}
---
+++ java-study-socket/src/com/hzqing/socket1/DemoServer.java
@@ -0,0 +1,13 @@
+package com.hzqing.socket1;
+
+import java.net.Socket;
+
+/**
+ * @author hzqing
+ * @date 2019-07-16 07:39
+ */
+public class ServerSocket {
+    public static void main(String[] args) {
+        Socket socket = new Socket()
+    }
+}
