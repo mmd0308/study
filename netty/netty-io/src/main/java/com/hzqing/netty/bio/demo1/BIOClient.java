450b163b045ce54d6b19b310006817d17bfd100a
hengzhaoqing <hengzhaoqing@163.com>
2019-12-17 16:57:44 +0800

    io

--- netty/netty-io/src/main/java/com/hzqing/netty/bio/demo1/BIOClient.java
+++ netty/netty-io/src/main/java/com/hzqing/netty/bio/demo1/BIOClient.java
@@ -1,4 +1,4 @@
-package com.hzqing.netty.bio;
+package com.hzqing.netty.bio.demo1;
 
 import java.io.IOException;
 import java.io.OutputStream;
--- netty/netty-io/src/main/java/com/hzqing/netty/bio/tomcat/TomcatApplication.java
+++ netty/netty-io/src/main/java/com/hzqing/netty/bio/tomcat/TomcatApplication.java
@@ -5,4 +5,27 @@ package com.hzqing.netty.bio.tomcat;
  * @date 2019-12-16 08:07
  */
 public class TomcatApplication {
+
+
+    // 1. 配置好端口  默认端口是8080  ServerSocket IP:localhost
+
+    // 2。web.xml 文件  servlet-name  servlet-class url-pattern
+        // Servlet集成HttpServlet
+
+    // 3. 读取配置文件 url-pattern 和servlet建立映射关系
+
+    // 4. Http请求，发送的数据就是字符串 HTTP协议
+
+
+    // 5。 从协议中获取URL，把响应的Servlet用反射进行实例化
+
+
+    // 6. 调用实例的servie方法，执行具体的逻辑
+
+
+    // 7. Request/Response
+
+
+
+
 }
